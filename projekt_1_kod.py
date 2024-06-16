b8de78f9e83b2a7994ed5e442942678800fe119c
tomas-tikal <tikal@3t.cz>
2024-06-16 15:32:19 +0200

    Add files via upload

--- projekt_1_kod.py
+++ projekt_1_kod.py
@@ -7,35 +7,9 @@ email: tikal@3t.cz
 discord: ti_to_80805
 """
 import string
-from projekt_1_knihovna import *
 
-TEXTS = ['''
-Situated about 10 miles west of Kemmerer,
-Fossil Butte is a ruggedly impressive
-topographic feature that rises sharply
-some 1000 feet above Twin Creek Valley
-to an elevation of more than 7500 feet
-above sea level. The butte is located just
-north of US 30N and the Union Pacific Railroad,
-which traverse the valley. ''',
-'''At the base of Fossil Butte are the bright
-red, purple, yellow and gray beds of the Wasatch
-Formation. Eroded portions of these horizontal
-beds slope gradually upward from the valley floor
-and steepen abruptly. Overlying them and extending
-to the top of the butte are the much steeper
-buff-to-white beds of the Green River Formation,
-which are about 300 feet thick.''',
-'''The monument contains 8198 acres and protects
-a portion of the largest deposit of freshwater fish
-fossils in the world. The richest fossil fish deposits
-are found in multiple limestone layers, which lie some
-100 feet below the top of the butte. The fossils
-represent several varieties of perch, as well as
-other freshwater genera and herring similar to those
-in modern oceans. Other fish such as paddlefish,
-garpike and stingray are also present.'''
-]
+import projekt_1_knihovna
+from projekt_1_knihovna import *
 
 # seznam existujicich registrovanych uzivatelu (slovnik VSTUP)
 uzivatele = {"bob": "pass", "ann": "pass123", "mike": "password123", "liz": "pass123"}
@@ -52,33 +26,28 @@ if(not porovnej_slovnik(uzivatele, prihlaseni)):
 else:
     print("GRATULUJI registrovany uzivateli, vstup do aplikace POVOLEN :)")
 
-print("Nyni vyber jednu z variant textu nize. Povolene hodnoty jsou cislice 1,2 nebo 3: ")
+# print("Nyni vyber jednu z variant textu nize. Povolene hodnoty jsou cislice 1,2 nebo 3: ")
 
 #  vstup od uzivatele, otestovani povolenych hodnot a ulozeni volby do 'seznam_slov'
 while True:
-    volba = input("Vloz cislici 1,2,3 nebo písmeno Q pro konec: \n")
-    match volba:
-        case "1":
-            print("Vlozena cislice 1, jdu dal ... \n")
-            seznam_slov = str.split(TEXTS[0])
-            break
-        case "2":
-            print("Vlozena cislice 2, jdu dal ... \n")
-            seznam_slov = str.split(TEXTS[1])
-            break
-        case "3":
-            print("Vlozena cislice 3, jdu dal ... \n")
-            seznam_slov = str.split(TEXTS[2])
-            break
-        case "q":
-            print("Vlozeno písmeno q, program je ukocen.")
-            exit()
-        case "Q":
-            print("Vlozeno písmeno Q, program je ukocen.")
-            exit()
-        case default:
-            print("Zkus to znova :) \n")
-            continue
+    pocet_textu = len(projekt_1_knihovna.TEXTS)
+    print("zadej volbu od 1 do", pocet_textu , "nebo písmeno Q pro konec: \n", end="")
+    volba = input()
+    povolene_volby = []
+    v = 1
+    while v <= pocet_textu:
+        povolene_volby.append(str(v))
+        v += 1
+    povolene_volby.append('q')
+    povolene_volby.append('Q')
+
+    if(not (volba in povolene_volby)):
+        print("Povolené volby jsou pouze:", povolene_volby)
+        print("Špatná volba, program končí")
+        exit()
+
+    seznam_slov = str.split(TEXTS[int(volba)-1])
+    break
 
 # pocet slov
 print("Výpis obsahuje", len(seznam_slov), "slov ve vybraném textu.")
