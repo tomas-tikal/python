537eefa32e049b6b5225ebc429f86c2cba900b99
tomas-tikal <tikal@3t.cz>
2024-06-16 20:55:27 +0200

    Add files via upload

--- projekt_2_2_knihovna_rek.py
+++ projekt_2_2_knihovna_rek.py
@@ -4,18 +4,18 @@ pole_num = [0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17, 20, 21, 22, 
 sousedi = [-11, -10, -9, -1, 1, 9, 10, 11]
 # sousedi_sousedu = [-22, -20, -18, -2, 2, 20, 22]
 
-def vstup(sachovnicka, hrac):
+def vstup(sachovnicka, hrac, velikost):
     while True:
         print("Zadej pozici na šachovnici (hraje:", hrac, ")")
-        radek = input("Zadej řádek (hodnota 0-7): ")
-        sloupec = input("Zadej sloupec (hodnota 0-7): ")
+        radek = input("Zadej číslo řádku: ")
+        sloupec = input("Zadej číslo sloupce: ")
 
-        if (not (radek.isnumeric() and (int(radek) >= 0 and int(radek) <= 7))):
-            print("Nezadali jste platný vstup - hodnota 0-7 - zkuste to znovu")
+        if (not (radek.isnumeric() and (int(radek) >= 0 and int(radek) <= int(velikost)))):
+            print("Nezadali jste platný vstup - levé číslo v daném poli - zkuste to znovu")
             continue
 
-        if (not (sloupec.isnumeric() and (int(sloupec) >= 0 and int(sloupec) <= 7))):
-            print("Nezadali jste platný vstup - hodnota 0-7 - zkuste to znovu")
+        if (not (sloupec.isnumeric() and (int(sloupec) >= 0 and int(sloupec) <= int(velikost)))):
+            print("Nezadali jste platný vstup - pravé číslo v daném poli - zkuste to znovu")
             continue
 
         if((hrac == 'X') and  (sachovnicka[int(radek)][int(sloupec)] == '.O.')):
@@ -29,9 +29,9 @@ def vstup(sachovnicka, hrac):
         break
     return radek, sloupec
 
-def vypis_sachovnice(vstup):
+def vypis_sachovnice(vstup, velikost):
     i = 0
-    while i < 8:
+    while i < int(velikost):
         print(vstup[i])
         i += 1
 
--- projekt_2_2_kod_rek.py
+++ projekt_2_2_kod_rek.py
@@ -7,29 +7,35 @@ email: tikal@3t.cz
 discord: ti_to_80805
 """
 import projekt_2_2_knihovna_rek
+while True:
+    velikost_sachovnice = input("Zadej velikost strany šachového pole pro hru: ")
+    if(not velikost_sachovnice.isdecimal()):
+        print('\"Velikost strany\" musí být CELÉ KLADNÉ číslo, zkus to znova :)')
+        continue
+    break
 
 # naplnění prázdné tabulky 8x8 buněk
 sachovnice= []
-for j in range(8):
+for j in range(int(velikost_sachovnice)):
     sloupecek= []
-    for i in range(8):
+    for i in range(int(velikost_sachovnice)):
         retezec = str(j)+'.'+str(i)
         sloupecek.append(retezec)
     sachovnice.append(sloupecek)
 
 # výpis prázdné šachovnice
-projekt_2_2_knihovna_rek.vypis_sachovnice(sachovnice)
+projekt_2_2_knihovna_rek.vypis_sachovnice(sachovnice, velikost_sachovnice)
 
 while True:
     #radek, sloupec = projekt_2_2_knihovna_rek.vstup_krizek(sachovnice)
-    radek, sloupec = projekt_2_2_knihovna_rek.vstup(sachovnice, 'X')
+    radek, sloupec = projekt_2_2_knihovna_rek.vstup(sachovnice, 'X', velikost_sachovnice)
     sachovnice[int(radek)][int(sloupec)] = '.X.'
-    projekt_2_2_knihovna_rek.vypis_sachovnice(sachovnice)
+    projekt_2_2_knihovna_rek.vypis_sachovnice(sachovnice, velikost_sachovnice)
     projekt_2_2_knihovna_rek.kontrola_souseda(sachovnice, radek, sloupec, '.X.')
     #radek, sloupec = projekt_2_2_knihovna_rek.vstup_kolecko(sachovnice)
-    radek, sloupec = projekt_2_2_knihovna_rek.vstup(sachovnice, '0')
+    radek, sloupec = projekt_2_2_knihovna_rek.vstup(sachovnice, '0', velikost_sachovnice)
     sachovnice[int(radek)][int(sloupec)] = '.O.'
-    projekt_2_2_knihovna_rek.vypis_sachovnice(sachovnice)
+    projekt_2_2_knihovna_rek.vypis_sachovnice(sachovnice, velikost_sachovnice)
     projekt_2_2_knihovna_rek.kontrola_souseda(sachovnice, radek, sloupec, '.O.')
 
 
