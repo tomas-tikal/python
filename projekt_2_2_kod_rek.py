05ff13011f2df8a69852fb91dff26bc8d4b7b6a8
tomas-tikal <tikal@3t.cz>
2024-06-16 21:07:41 +0200

    Add files via upload

--- projekt_2_2_kod_rek.py
+++ projekt_2_2_kod_rek.py
@@ -23,20 +23,29 @@ for j in range(int(velikost_sachovnice)):
         sloupecek.append(retezec)
     sachovnice.append(sloupecek)
 
+# pomocná proměnná možných indexů v šachonici
+pole_num = []
+for j in range(int(velikost_sachovnice)):
+    for i in range(int(velikost_sachovnice)):
+        souradnice = str(j)+str(i)
+        pole_num.append(int(souradnice))
+
+print(pole_num)
+
 # výpis prázdné šachovnice
 projekt_2_2_knihovna_rek.vypis_sachovnice(sachovnice, velikost_sachovnice)
 
 while True:
     #radek, sloupec = projekt_2_2_knihovna_rek.vstup_krizek(sachovnice)
-    radek, sloupec = projekt_2_2_knihovna_rek.vstup(sachovnice, 'X', velikost_sachovnice)
+    radek, sloupec = projekt_2_2_knihovna_rek.vstup(sachovnice, 'X', velikost_sachovnice, pole_num)
     sachovnice[int(radek)][int(sloupec)] = '.X.'
     projekt_2_2_knihovna_rek.vypis_sachovnice(sachovnice, velikost_sachovnice)
-    projekt_2_2_knihovna_rek.kontrola_souseda(sachovnice, radek, sloupec, '.X.')
+    projekt_2_2_knihovna_rek.kontrola_souseda(sachovnice, radek, sloupec, '.X.', pole_num)
     #radek, sloupec = projekt_2_2_knihovna_rek.vstup_kolecko(sachovnice)
-    radek, sloupec = projekt_2_2_knihovna_rek.vstup(sachovnice, '0', velikost_sachovnice)
+    radek, sloupec = projekt_2_2_knihovna_rek.vstup(sachovnice, '0', velikost_sachovnice, pole_num)
     sachovnice[int(radek)][int(sloupec)] = '.O.'
     projekt_2_2_knihovna_rek.vypis_sachovnice(sachovnice, velikost_sachovnice)
-    projekt_2_2_knihovna_rek.kontrola_souseda(sachovnice, radek, sloupec, '.O.')
+    projekt_2_2_knihovna_rek.kontrola_souseda(sachovnice, radek, sloupec, '.O.', pole_num)
 
 
 
